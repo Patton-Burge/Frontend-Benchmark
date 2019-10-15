22fd0857c2d7d5ea8b1cb681490a26c985d44218
Nate Clark <natec425@gmail.com>
2019-10-14 22:25:31 -0500

    setup backend

---
+++ entries/views.py
@@ -0,0 +1,35 @@
+from django.views.generic import CreateView, ListView, View
+from django.views.generic.detail import SingleObjectMixin
+from django.urls import reverse_lazy
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Entry
+
+
+class LatestEntryView(ListView):
+    queryset = Entry.objects.all().order_by("-created_at")
+    paginate_by = 10
+    template_name = "latest_entries.html"
+    context_object_name = "entries"
+
+
+class TopEntriesView(ListView):
+    queryset = Entry.objects.all().order_by("-likes")
+    paginate_by = 10
+    template_name = "latest_entries.html"
+    context_object_name = "entries"
+
+
+class CreateEntryView(CreateView):
+    model = Entry
+    fields = ("content",)
+    success_url = reverse_lazy("latest_entries")
+    template_name = "create_entry.html"
+
+
+class LikeEntryView(View):
+    def post(self, request, id):
+        entry = get_object_or_404(Entry, id=id)
+        entry.likes += 1
+        entry.save()
+        return JsonResponse({"likes": entry.likes})
--- guestbook/urls.py
+++ guestbook/urls.py
@@ -1,21 +1,21 @@
-"""guestbook URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.views.generic import RedirectView
+from django.views.decorators.csrf import csrf_exempt
+from entries.views import (
+    CreateEntryView,
+    LatestEntryView,
+    LikeEntryView,
+    TopEntriesView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", RedirectView.as_view(url=reverse_lazy("latest_entries"))),
+    path("entries/", LatestEntryView.as_view(), name="latest_entries"),
+    path("entries/top", TopEntriesView.as_view(), name="top_entries"),
+    path("entries/new", CreateEntryView.as_view(), name="create_entry"),
+    path(
+        "entries/<int:id>/like", csrf_exempt(LikeEntryView.as_view()), name="like_entry"
+    ),
+    path("admin/", admin.site.urls),
 ]
